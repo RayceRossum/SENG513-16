4f9366847b9288a46ea5025010856c8c4a8da3e1
Rayce Rossum <Rayce.Rossum@gmail.com>
2017-03-16 13:41:12 -0600

    Inital commit of basic password auth

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 var express = require('express');
 var app = express();
+var passport = require('passport');
+var Strategy = require('passport-http-bearer').Strategy;
+var db = require('./db');
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.findOne({
+            username: username
+        }, function(err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false);
+            }
+            if (!user.verifyPassword(password)) {
+                return done(null, false);
+            }
+            return done(null, user);
+        });
+    }
+));
+
+// Configure Express application.
+app.use(require('morgan')('combined'));
+
+
+app.post('/login',
+    passport.authenticate('local', {
+        failureRedirect: '/login'
+    }),
+    function(req, res) {
+        res.redirect('/');
+    });
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,11 +44,13 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
+});
+
+app.get('/db', function(request, response) {
+    response.render('pages/db');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
