560f77549aff24993716cb6268e957bfec8b406f
Rayce Rossum <Rayce.Rossum@gmail.com>
2017-03-28 12:04:45 -0600

    Add form validation to buyers page, server and client now require Item

--- public/js/handelers.js
+++ public/js/handelers.js
@@ -6,110 +6,110 @@ var buyerLocation;
 $(document).on("pageload")
 
 $(document).ready(function() {
-    
+
     $.ajax({
         type: "GET",
         url: "/getRecentAds",
-        success: function(data){
+        success: function(data) {
             var jsonObj = JSON.parse(data);
-            
+
             $('#listings').empty();
-            
-            $('#prevbtn').prop('disabled',true);
-            
-            if(jsonObj[0] === "true"){
-                $('#nextbtn').prop('disabled',true);
+
+            $('#prevbtn').prop('disabled', true);
+
+            if (jsonObj[0] === "true") {
+                $('#nextbtn').prop('disabled', true);
             }
-            
-            for (var i = 0; i < jsonObj[1].length; i++){
+
+            for (var i = 0; i < jsonObj[1].length; i++) {
                 $('#listings').append('<li class="list-group-item row">' +
-                                      '<div class="col-md-4">' + jsonObj[1][i].item + '</div>' +
-                                      '<div class="col-md-4">' + jsonObj[1][i].buyerLoc + '</div>' +
-                                      '<div class="col-md-4"><a class="openListing" href="#" data-id="' + jsonObj[1][i].id + '" data-toggle="modal" data-target="#bannerformmodal">More</a></div>');
+                    '<div class="col-md-4">' + jsonObj[1][i].item + '</div>' +
+                    '<div class="col-md-4">' + jsonObj[1][i].buyerLoc + '</div>' +
+                    '<div class="col-md-4"><a class="openListing" href="#" data-id="' + jsonObj[1][i].id + '" data-toggle="modal" data-target="#bannerformmodal">More</a></div>');
             }
         }
-        
-    });
-    
-    $('#nextbtn').on('click', function(e){
-        pgnumber++;
-       $.ajax({
-           type: "POST",
-           url: "/getPage",
-           data: {
-               pagenum: pgnumber,
-               isfiltered: filtered,
-               itemLocation: itemLocation,
-               buyerLocation: buyerLocation
-           },
-           success: function(data){
-               var jsonObj = JSON.parse(data);
-               
-               $('#listings').empty();
-               
-               if(jsonObj[0] == "true"){
-                   $('#nextbtn').prop('disabled',true);
-               }
-               
-               for (var i = 0; i < jsonObj[1].length; i++){
-                   $('#listings').append('<li class="list-group-item row">' +
-                                         '<div class="col-md-4">' + jsonObj[1][i].item + '</div>' +
-                                         '<div class="col-md-4">' + jsonObj[1][i].buyerLoc + '</div>' +
-                                         '<div class="col-md-4"><a class="openListing" href="#" data-id="' + jsonObj[1][i].id + '" data-toggle="modal" data-target="#bannerformmodal">More</a></div>');
-               }
-               $('#prevbtn').prop('disabled',false);
-           }
-       });
-        
-    });
-    
-    $('#prevbtn').on('click', function(e){
-        pgnumber--;
-            
-       $.ajax({
-           type: "POST",
-           url: "/getPage",
-           data: {
-               pagenum: pgnumber,
-               isfiltered: filtered,
-               itemLocation: itemLocation,
-               buyerLocation: buyerLocation
-           },
-           success: function(data){
-               var jsonObj = JSON.parse(data);
-               
-               
-               $('#listings').empty();
-               
-               if(jsonObj[0] == "true"){
-                   $('#prevbtn').prop('disabled',true);
-               }
-               
-               for (var i = 0; i < jsonObj[1].length; i++){
-                   $('#listings').append('<li class="list-group-item row">' +
-                                         '<div class="col-md-4">' + jsonObj[1][i].item + '</div>' +
-                                         '<div class="col-md-4">' + jsonObj[1][i].buyerLoc + '</div>' +
-                                         '<div class="col-md-4"><a class="openListing" href="#" data-id="' + jsonObj[1][i].id + '" data-toggle="modal" data-target="#bannerformmodal">More</a></div>');
-               }
-               $('#nextbtn').prop('disabled',false);
-               
-           }
-       });
-        
-    });
-    
-     $('#filterListings').submit(function(event) {
-         event.preventDefault();
-         
-         var form = $('#filterListings')[0];
-         var formData = new FormData(form);
-         
-         buyerLocation = $('#buyerLoc').val();
-         itemLocation = $('#itemLoc').val();
-         
-         pgnumber = 0;
 
-         $.ajax({
+    });
+
+    $('#nextbtn').on('click', function(e) {
+        pgnumber++;
+        $.ajax({
+            type: "POST",
+            url: "/getPage",
+            data: {
+                pagenum: pgnumber,
+                isfiltered: filtered,
+                itemLocation: itemLocation,
+                buyerLocation: buyerLocation
+            },
+            success: function(data) {
+                var jsonObj = JSON.parse(data);
+
+                $('#listings').empty();
+
+                if (jsonObj[0] == "true") {
+                    $('#nextbtn').prop('disabled', true);
+                }
+
+                for (var i = 0; i < jsonObj[1].length; i++) {
+                    $('#listings').append('<li class="list-group-item row">' +
+                        '<div class="col-md-4">' + jsonObj[1][i].item + '</div>' +
+                        '<div class="col-md-4">' + jsonObj[1][i].buyerLoc + '</div>' +
+                        '<div class="col-md-4"><a class="openListing" href="#" data-id="' + jsonObj[1][i].id + '" data-toggle="modal" data-target="#bannerformmodal">More</a></div>');
+                }
+                $('#prevbtn').prop('disabled', false);
+            }
+        });
+
+    });
+
+    $('#prevbtn').on('click', function(e) {
+        pgnumber--;
+
+        $.ajax({
+            type: "POST",
+            url: "/getPage",
+            data: {
+                pagenum: pgnumber,
+                isfiltered: filtered,
+                itemLocation: itemLocation,
+                buyerLocation: buyerLocation
+            },
+            success: function(data) {
+                var jsonObj = JSON.parse(data);
+
+
+                $('#listings').empty();
+
+                if (jsonObj[0] == "true") {
+                    $('#prevbtn').prop('disabled', true);
+                }
+
+                for (var i = 0; i < jsonObj[1].length; i++) {
+                    $('#listings').append('<li class="list-group-item row">' +
+                        '<div class="col-md-4">' + jsonObj[1][i].item + '</div>' +
+                        '<div class="col-md-4">' + jsonObj[1][i].buyerLoc + '</div>' +
+                        '<div class="col-md-4"><a class="openListing" href="#" data-id="' + jsonObj[1][i].id + '" data-toggle="modal" data-target="#bannerformmodal">More</a></div>');
+                }
+                $('#nextbtn').prop('disabled', false);
+
+            }
+        });
+
+    });
+
+    $('#filterListings').submit(function(event) {
+        event.preventDefault();
+
+        var form = $('#filterListings')[0];
+        var formData = new FormData(form);
+
+        buyerLocation = $('#buyerLoc').val();
+        itemLocation = $('#itemLoc').val();
+
+        pgnumber = 0;
+
+        $.ajax({
             type: "POST",
             enctype: 'multipart/form-data',
             url: "/filterListings", //path of url where u want to submit form
@@ -118,38 +118,40 @@ $(document).ready(function() {
             contentType: false,
             cache: false,
             success: function(data) {
-                
+
                 var jsonObj = JSON.parse(data);
-                
+
                 $('#listings').empty();
 
-                for (var i = 0; i < jsonObj[1].length; i++){
+                for (var i = 0; i < jsonObj[1].length; i++) {
                     $('#listings').append('<li class="list-group-item row">' +
-                                        '<div class="col-md-4">' + jsonObj[1][i].item + '</div>' +
-                                        '<div class="col-md-4">' + jsonObj[1][i].buyerLoc + '</div>' +
-                                        '<div class="col-md-4"><a class="openListing" href="#" data-id="' + jsonObj[1][i].id + '" data-toggle="modal" data-target="#bannerformmodal">More</a></div>');
+                        '<div class="col-md-4">' + jsonObj[1][i].item + '</div>' +
+                        '<div class="col-md-4">' + jsonObj[1][i].buyerLoc + '</div>' +
+                        '<div class="col-md-4"><a class="openListing" href="#" data-id="' + jsonObj[1][i].id + '" data-toggle="modal" data-target="#bannerformmodal">More</a></div>');
                 }
-                
+
                 filtered = "true";
-                $('#prevbtn').prop('disabled',true);
-                
-                if(jsonObj[0] === "true")
-                $('#nextbtn').prop('disabled',true);
+                $('#prevbtn').prop('disabled', true);
+
+                if (jsonObj[0] === "true")
+                    $('#nextbtn').prop('disabled', true);
                 else
-                $('#nextbtn').prop('disabled',false);  
+                    $('#nextbtn').prop('disabled', false);
             }
         });
-     });
+    });
 
-    $(document).on("click", ".openListing", function () {
+    $(document).on("click", ".openListing", function() {
 
         var listId = $(this).data('id');
 
         $.ajax({
             type: "POST",
             url: "/getAdDetails",
-            data: {'listingId': listId},
-            success: function(data){
+            data: {
+                'listingId': listId
+            },
+            success: function(data) {
 
                 var jsonObj = JSON.parse(data);
 
@@ -164,13 +166,13 @@ $(document).ready(function() {
                 $('#listItem').append(jsonObj.item);
                 $('#listBuyerLoc').append(jsonObj.buyerLoc);
 
-                if(jsonObj.itemLoc)
+                if (jsonObj.itemLoc)
                     $('#listItemLoc').append(jsonObj.itemLoc);
-                if(jsonObj.imagedata)
+                if (jsonObj.imagedata)
                     $('#listImage').append(jsonObj.imagedata);
-                if(jsonObj.details)
+                if (jsonObj.details)
                     $('#listDetails').append(jsonObj.details);
-        }
+            }
         });
 
     });
--- public/js/postad.js
+++ public/js/postad.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
-    
+
     $.ajax({
         type: "GET",
         url: "/getStats",
-        success: function(data){
+        success: function(data) {
             var jsonObj = JSON.parse(data);
-            
-            if (jsonObj[1][0]){
+
+            if (jsonObj[1][0]) {
                 $('#item1').empty();
                 $('#item1').append(jsonObj[1][0]);
             }
-            if (jsonObj[1][1]){
+            if (jsonObj[1][1]) {
                 $('#item2').empty();
                 $('#item2').append(jsonObj[1][1]);
             }
-            if (jsonObj[1][2]){
+            if (jsonObj[1][2]) {
                 $('#item3').empty();
                 $('#item3').append(jsonObj[1][2]);
             }
-            if (jsonObj[1][3]){
+            if (jsonObj[1][3]) {
                 $('#item4').empty();
                 $('#item4').append(jsonObj[1][3]);
             }
-            if (jsonObj[1][4]){
+            if (jsonObj[1][4]) {
                 $('#item5').empty();
                 $('#item5').append(jsonObj[1][4]);
             }
-            
+
         }
-        
+
     });
 
     $("#imagePreview").error(function() {
@@ -54,31 +54,33 @@ $(document).ready(function() {
         }
     }
 
-    $('#uploadAd').submit(function(event) {
-        event.preventDefault();
+    $("#uploadAd").validate({
+        submitHandler: function() {
+            event.preventDefault();
 
-        var form = $('#uploadAd')[0];
-        var formData = new FormData(form);
+            var form = $('#uploadAd')[0];
+            var formData = new FormData(form);
 
-        $.ajax({
-            type: "POST",
-            enctype: 'multipart/form-data',
-            url: "/submitAd", //path of url where u want to submit form
-            data: formData,
-            processData: false,
-            contentType: false,
-            cache: false,
-            success: function(data) {
-                if (data === "true") {
-                    $('#uploadAd').before('<div class="alert alert-success alert-dismissable">' + '<a href="#"' + 'class="close" data-dismiss="alert" aria-label="close">&times;</a>' + '<strong>Success!</strong> Your new post has successfully been added. Please wait for a registered Handeler to accept' + '</div>');
-                    $('#item').val('');
-                    $('#image').val('');
-                    $('#details').val('');
-                    $('#imagePreview').hide();
-                } else {
-                    $('#uploadAd').before('<div class="alert alert-danger alert-dismissable">' + '<a href="#"' + 'class="close" data-dismiss="alert" aria-label="close">&times;</a>' + '<strong>Error!</strong> Please ensure the fields are populated correctly' + '</div>');
+            $.ajax({
+                type: "POST",
+                enctype: 'multipart/form-data',
+                url: "/submitAd", //path of url where u want to submit form
+                data: formData,
+                processData: false,
+                contentType: false,
+                cache: false,
+                success: function(data) {
+                    if (data === "true") {
+                        $('#uploadAd').before('<div class="alert alert-success alert-dismissable">' + '<a href="#"' + 'class="close" data-dismiss="alert" aria-label="close">&times;</a>' + '<strong>Success!</strong> Your new post has successfully been added. Please wait for a registered Handeler to accept' + '</div>');
+                        $('#item').val('');
+                        $('#image').val('');
+                        $('#details').val('');
+                        $('#imagePreview').hide();
+                    } else {
+                        $('#uploadAd').before('<div class="alert alert-danger alert-dismissable">' + '<a href="#"' + 'class="close" data-dismiss="alert" aria-label="close">&times;</a>' + '<strong>Error!</strong> Please ensure the fields are populated correctly' + '</div>');
+                    }
                 }
-            }
-        });
+            });
+        }
     });
 });
