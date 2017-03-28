f89918e05b211171df6b72cdbe93bec4acc421cd
Rayce Rossum <Rayce.Rossum@gmail.com>
2017-03-28 12:48:37 -0600

    Transfer js for stats to home

--- public/js/postad.js
+++ public/js/postad.js
@@ -1,48 +1,5 @@
 $(document).ready(function() {
 
-    $.ajax({
-        type: "GET",
-        url: "/getStats",
-        success: function(data) {
-            var jsonObj = JSON.parse(data);
-
-            if (jsonObj[0][0]){
-                $('#nUsers').empty();
-                $('#nUsers').append(jsonObj[0][0]);
-            }
-            if (jsonObj[0][1]){
-                $('#nHandelers').empty();
-                $('#nHandelers').append(jsonObj[0][1]);                
-            }
-            if (jsonObj[0][2]){
-                $('#nAds').empty();
-                $('#nAds').append(jsonObj[0][2]);                
-            }
-            if (jsonObj[1][0]){
-                $('#item1').empty();
-                $('#item1').append(jsonObj[1][0]);
-            }
-            if (jsonObj[1][1]) {
-                $('#item2').empty();
-                $('#item2').append(jsonObj[1][1]);
-            }
-            if (jsonObj[1][2]) {
-                $('#item3').empty();
-                $('#item3').append(jsonObj[1][2]);
-            }
-            if (jsonObj[1][3]) {
-                $('#item4').empty();
-                $('#item4').append(jsonObj[1][3]);
-            }
-            if (jsonObj[1][4]) {
-                $('#item5').empty();
-                $('#item5').append(jsonObj[1][4]);
-            }
-
-        }
-
-    });
-
     $("#imagePreview").error(function() {
         $(this).hide();
     });
