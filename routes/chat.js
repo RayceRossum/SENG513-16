22a30deffee70b87c3a4be7778689d7e1f73a6f6
Rayce Rossum <Rayce.Rossum@gmail.com>
2017-03-29 11:31:15 -0600

    Add basic chat page, css, and router

--- routes/chat.js
+++ routes/chat.js
@@ -1,4 +1,15 @@
 module.exports = function(express, query, db) {
     var router = express.Router();
+
+    router.get('/chat', function(request, response) {
+        if (request.user) {
+            response.render('pages/chat', {
+                user: request.user
+            });
+        } else {
+            response.redirect('/');
+        }
+    });
+
     return router;
 }
