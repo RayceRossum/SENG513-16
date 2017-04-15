51b8cf2215ca41c4b0eac56ef94684a3770764a2
ZoeKing <zk@cdzking.com>
2017-04-15 16:43:02 -0600

    User profile implemented

--- public/js/profiles.js
+++ public/js/profiles.js
@@ -1,3 +1,18 @@
-function loadProfileData() {
-    alert("load data");
-}
+$(document).ready(function(){
+  $("#my-profile-button").on("click", function(){
+    var username = $(this).data('username');
+
+    $.ajax({
+        type: "POST",
+        url: "/getProfile",
+        data: {
+            username: username
+        },
+        success: function(profile) {
+          $('#myProfileModal .profile-username').text(profile.username);
+          $('#myProfileModal .profile-country').text(profile.country);
+          $('#myProfileModal .profile-rating').text(profile.rating);
+        }
+    });
+  });
+});
--- routes/profiles.js
+++ routes/profiles.js
@@ -3,8 +3,8 @@ module.exports = function(express, query, db) {
 
     router.post('/getProfile', function(request, response) {
         if (request.user) {
-          if (request.body.profileUsername) {
-            var profileData = db.profiles.getProfile(query, request.body.profileUsername, function(err, userData) {
+          if (request.body.username) {
+            var profileData = db.profiles.getProfile(query, request.body.username, function(err, userData) {
                 if (err) {
                     console.error(err);
                 } else {
@@ -15,19 +15,6 @@ module.exports = function(express, query, db) {
         } else {
             response.redirect('/');
         }
-    });
-
-    router.post('/getMessages', function(request, response) {
-      if (request.user) {
-        if (request.body.conversationID) {
-          var messages = db.messages.getMessages(request.body.conversationID, query, function(err, result) {
-            console.log(result);
-            response.status(200).send(result);
-          });
-        }
-      } else {
-        response.redirect('/');
-      }
     });
 
     return router;
