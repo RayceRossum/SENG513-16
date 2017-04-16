ab3d3240adaeac6ef23b07c7f5c1432dd129d685
Rayce Rossum <Rayce.Rossum@gmail.com>
2017-04-16 11:39:35 -0600

    Fix merge issues

--- public/js/profiles.js
+++ public/js/profiles.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
-  $("#my-profile-button").on("click", function(){
-    var username = $(this).data('username');
+$(document).ready(function() {
+    $("#my-profile-button").on("click", function() {
+        var username = $(this).data('username');
 
-    $.ajax({
-        type: "POST",
-        url: "/getProfile",
-        data: {
-            username: username
-        },
-        success: function(profile) {
-          console.log(profile);
-          $('#myProfileModal .profile-username').text(profile.username);
-          $('#myProfileModal .profile-type').text(profile.accounttype);
-          $('#myProfileModal .profile-country').text(profile.country);
-          $('#myProfileModal .profile-rating').text(profile.rating);
-        }
+        $.ajax({
+            type: "POST",
+            url: "/getProfile",
+            data: {
+                username: username
+            },
+            success: function(profile) {
+                console.log(profile);
+                $('#myProfileModal .profile-username').text(profile.username);
+                $('#myProfileModal .profile-type').text(profile.accounttype);
+                $('#myProfileModal .profile-country').text(profile.country);
+                $('#myProfileModal .profile-rating').text(profile.rating);
+            }
+        });
     });
-  });
 });
