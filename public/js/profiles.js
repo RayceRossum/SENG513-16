6c014cdf245496274b9494312b7c9ac5e68d3434
Rayce Rossum <Rayce.Rossum@gmail.com>
2017-04-16 12:02:30 -0600

    Hide rating if showing a buyer

--- public/js/profiles.js
+++ public/js/profiles.js
@@ -9,10 +9,15 @@ $(document).ready(function() {
                 username: username
             },
             success: function(profile) {
+                $('#profileRating').show();
                 $('#myProfileModal .profile-username').text(profile.username);
                 $('#myProfileModal .profile-type').text(profile.accounttype.charAt(0).toUpperCase() + profile.accounttype.slice(1));
                 $('#myProfileModal .profile-country').text(profile.country);
-                $('#myProfileModal .profile-rating').text(parseInt(profile.handelerrating)/parseInt(profile.totalratings));
+                if (profile.accounttype === "handeler") {
+                    $('#myProfileModal .profile-rating').text(parseInt(profile.handelerrating) / parseInt(profile.totalratings));
+                } else {
+                    $('#profileRating').hide();
+                }
             }
         });
     });
